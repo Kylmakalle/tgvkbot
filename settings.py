f562c8057f26f4da6dece3b87cb4e26e1688f104
dm1sh <me@dmitriy.icu>
2022-06-20 13:41:22 +0300

    Made DATABASE_URL parsing conditional

--- settings.py
+++ settings.py
@@ -4,11 +4,13 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL)
-}
 
-if not DATABASE_URL:
+DATABASES = {}
+
+if DATABASE_URL:
+    # Reads string from DATABASE_URL env by default
+    DATABASES['default'] = dj_database_url.config()
+else:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': DATABASE_NAME,
